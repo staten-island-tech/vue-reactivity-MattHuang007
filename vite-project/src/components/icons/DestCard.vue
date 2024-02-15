06f872a2b9735c6bc313e720da7ee60f235646c1
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-02-15 13:29:33 -0500

    b

--- vite-project/src/components/icons/DestCard.vue
+++ vite-project/src/components/icons/DestCard.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
 <h2>{{ Destination.name }}</h2>
+
 <img :src="Destination.name" alt=""/>
-<h3></h3>
+<h3>
+
+</h3>
 <button @clicked="increment">
     Click Me
 </button>
     </div>
 </template>
+
 <script setup>
-import {ref} from "vue";
+/* import {ref} from "vue";
 const props= defineProps({
     Destination: Object
 });
@@ -17,7 +21,31 @@ const props= defineProps({
 const clicked= ref(0);
 function increment(){
     clicked.value++;
-}
+} */
+const pictures = [
+"./pictures/pckl.jpg",
+"./pictures/wwbread.jpg",
+"./pictures/amrcn.jpg",
+"./pictures/bcon.jpg",
+"./pictures/breadpudding.jpg",
+"./pictures/breadroll.jpg",
+"./pictures/chddr.jpg",
+"./pictures/croco.jpg",
+"./pictures/disgustingahh.jpg",
+"./pictures/ham.jpg",
+"./pictures/htsc.jpg",
+"./pictures/jlly.jpg",
+"./pictures/ktchp.jpg",
+"./pictures/lttc.jpg",
+"./pictures/mayo.jpg",
+"./pictures/mstrd.jpg",
+"./pictures/nasty.jpg",
+"./pictures/pntbttr.jpg",
+"./pictures/swiss.jpg",
+"./pictures/tmto.jpg",
+"./pictures/trkey.jpg",
+"./pictures/whitebread.jpg",
+]
 </script>
 
 <style scoped>
