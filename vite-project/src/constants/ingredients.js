1851e2cdbe65113cde815da0715265589dfbe23f
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-03-06 12:15:48 -0500

    Update ingredients.js

--- vite-project/src/constants/ingredients.js
+++ vite-project/src/constants/ingredients.js
@@ -2,71 +2,71 @@
 export const INGREDIENTS = [
   {
     name: "White Bread",
-    src: "http://localhost:5173/Imgs/whitebread.jpg"
+    src: "https://www.goldmedalbakery.com/content/uploads/2019/12/Jumbo-22oz-White.jpg"
   },
   {
     name: "Whole Wheat Bread",
-    src: "Imgs/wwbread.jpg"
+    src: "https://recipeland.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsiZGF0YSI6MTY3ODUsInB1ciI6ImJsb2JfaWQifX0=--93b348f1e44e27d4d4a6ee0a4d7aa9ee031ab1a6/eyJfcmFpbHMiOnsiZGF0YSI6eyJmb3JtYXQiOiJqcGciLCJyZXNpemVfdG9fbGltaXQiOls4NjAsbnVsbF0sImNvbnZlcnQiOiJ3ZWJwIiwic2F2ZXIiOnsic3Vic2FtcGxlX21vZGUiOiJvbiIsInN0cmlwIjpmYWxzZSwiaW50ZXJsYWNlIjp0cnVlLCJxdWFsaXR5Ijo1MH19LCJwdXIiOiJ2YXJpYXRpb24ifX0=--4288720e597eeb129da100809b95192d1d38cc9d/orig_7a708f68443a39c5865a.jpg"
   },
   {
     name: "Bread Roll",
-    src: "Imgs/breadroll.jpg"
+    src: "https://handletheheat.com/wp-content/uploads/2014/06/hawaiian-bread-rolls-SQUARE.jpg"
   },{
     name: "Bread Pudding",
-    src: "Imgs/breadpudding.jpg"
+    src: "https://mojo.generalmills.com/api/public/content/nThKSsIDAU6y-lX6yXmvew_gmi_hi_res_jpeg.jpeg?v=7cc69354&t=466b54bb264e48b199fc8e83ef1136b4"
   },{
     name: "Ketchup",
-    src: "Imgs/ktchp.jpg"
+    src: "https://images.heb.com/is/image/HEBGrocery/001905374-1"
   },{
     name: "Mayo",
-    src: "Imgs/mayo.jpg"
+    src: "https://healthylittlepeach.com/wp-content/uploads/2019/03/homemade-mayo-60-second-1-2.jpg"
   },{
     name: "Mustard",
-    src: "Imgs/mstrd.jpg"
+    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUbNpFxu0wKN7gS568TaYU66b4GnyFFgSxGg&usqp=CAU"
   },{
     name: "Hot Sauce",
-    src: "Imgs/htsc.jpg"
+    src: "https://images.immediate.co.uk/production/volatile/sites/30/2022/07/Hot-Sauce2-ecdc728.jpg?quality=90&resize=556,505"
   },{
     name: "Jelly",
-    src: "Imgs/jlly.jpg"
+    src: "https://p-cdn6consumer.jmsinf.com/tmp/image-thumbnails/smuckers/products/fruit-spreads/image-thumb__5824__auto_feab9401f1ce62dd47e2128655684d97/smckr-strawberry-jelly-18oz-1920x1920.jpg"
   },{
     name: "Peanut Butter",
-    src: "Imgs/pntbttr.jpg"
+    src: "https://hips.hearstapps.com/hmg-prod/images/peanut-butter-vegan-1556206811.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*"
   },{
     name: "Cheddar Cheese",
-    src: "Imgs/chddr.jpg"
+    src: "https://131998451.cdn6.editmysite.com/uploads/1/3/1/9/131998451/s174072524441932549_p52_i1_w600.png"
   },{
     name: "Swiss Cheese",
-    src: "Imgs/swiss.jpg"
+    src: "https://pearlvalleycheese.com/cdn/shop/products/mild-swiss-cheese_800x.jpg?v=1676472796"
   },{
     name: "American Cheese",
-    src: "Imgs/amrcn.jpg"
+    src: "https://www.allrecipes.com/thmb/iRcip7RaCHF_RmbzDV4bC8gqNYs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-504780334-2000-1caaee9e697c40a8935db7b8d9818a85.jpg"
   },{
     name: "Blue Cheese",
-    src: "Imgs/nasty.jpg"
+    src: "https://cdn.britannica.com/21/78721-050-E0525C8E/stilton-cheese.jpg"
   },{
     name: "Tomato",
-    src: "Imgs/tmto.jpg"
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tomato_je.jpg/1200px-Tomato_je.jpg"
   },{
     name: "Pickles",
-    src: "Imgs/pckl.jpg"
+    src: "https://www.gimmesomeoven.com/wp-content/uploads/2016/07/Homemade-Pickles-Recipe-9.jpg"
   },{
     name: "Lettuce",
-    src: "Imgs/lttc.jpg"
+    src: "https://www.100daysofrealfood.com/wp-content/uploads/2023/11/vecteezy_lettuce-salad-leaf-isolated-on-white-background-with_5582269-scaled.jpg"
   },{
     name: "Arugula",
-    src: "Imgs/disgustingahh.jpg"
+    src: "https://www.liveeatlearn.com/wp-content/uploads/2021/05/arugula-photo-4.jpg"
   },{
     name: "Turkey",
-    src: "Imgs/trkey.jpg"
+    src: "https://www.halandalmeats.com/cdn/shop/files/turkeymeat.png?v=1686330288&width=416"
   },{
     name: "Ham",
-    src: "Imgs/ham.jpg"
+    src: "https://cdn.vox-cdn.com/thumbor/PtOpdbXq2KbFQPM6baWSoSjS1fM=/0x0:4452x2968/1200x800/filters:focal(1870x1128:2582x1840)/cdn.vox-cdn.com/uploads/chorus_image/image/63145123/shutterstock_1109532236.0.jpg"
   },{
     name: "Bacon",
-    src: "Imgs/bcon.jpg"
+    src: "https://www.wellplated.com/wp-content/uploads/2023/06/Baked-bacon-in-the-oven.jpg"
   },{
     name: "Crocodile",
-    src: "Imgs/croco.jpg"
+    src: "https://media-cdn.tripadvisor.com/media/photo-s/13/75/7e/28/rainbow-platter.jpg"
   },
 ];
